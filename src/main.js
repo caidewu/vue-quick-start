51a11d1abfadd529afe3adf5c86f646424d115ee
caidewu <caidewu555@gmail.com>
2018-11-12 18:04:23 +0800

    引入element组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './plugins/element';
+
+import './theme.scss';
 
 Vue.config.productionTip = false;
+
+console.log('BASE_URL:', process.env.BASE_URL);
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 new Vue({
   router,
---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+/**
+ * 动态加载Element组件
+ */
+import Vue from 'vue';
+import { Button } from 'element-ui';
+
+Vue.use(Button);
